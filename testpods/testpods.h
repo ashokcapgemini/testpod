f7b7896f759156e08e8ad5f5799bb5695c4fcb2c
ashokcapgemini <ashokkumar.ashokkumar@capgemini.com>
2021-09-20 20:37:47 +0530

    Initial val

---
+++ testpods/testpods.h
@@ -0,0 +1,18 @@
+//
+//  testpods.h
+//  testpods
+//
+//  Created by AshokKumar on 20/09/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for testpods.
+FOUNDATION_EXPORT double testpodsVersionNumber;
+
+//! Project version string for testpods.
+FOUNDATION_EXPORT const unsigned char testpodsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testpods/PublicHeader.h>
+
+
